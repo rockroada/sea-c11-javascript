6e0f9051fa2eebf0ff7a9974b021250e62b4084c
Yuliya Zakharenko <rockroada@gmail.com>
2014-04-27 22:57:56 -0700

    final commit. cleaned up the code

--- students/yuliyaz/tabs/script.js
+++ students/yuliyaz/tabs/script.js
@@ -1,34 +1,18 @@
 $(document).ready(function () {
     $.getJSON('http://rs.hankyates.com:3000/content', function(data){
-            //home = data.home;
-            //articles = data.articles
-            //portfolio = data.portfolio
-    //$.each(data, function(key, value){
-    //$('#home').html(home);
-    //$('#articles').html(articles);
-    //$('#portfolio').html(portfolio);
-   $.each(data, function(key, value){
-    //$(".tab_content_container").append(value);
+        $.each(data, function(key, value){
+            $(".tabs").append("<li><a href = #"+key +">"+key+"</a></li>");
+            $(".tab_content_container").append("<div class= 'tab_content', id = " + key +">"+value+"</div>");
+        });
 
-    //$('<div/>', {
-   //id: key,
-   //class: "tab_content",
-  //text: value
-//}).appendTo(".tab_content_container")
-$(".tabs").append("<li><a href = #"+key +">"+key+"</a></li>");
-$(".tab_content_container").append("<div class= 'tab_content', id = " + key +">"+value+"</div>");
-  });
+        //When page loads...
+        $("ul.tabs li:first-child a").addClass("active").show(); //Activate first tab
+        $(".tab_content:first-child").show(); //Show first tab content
 
-    //When page loads...
-$("ul.tabs li:first-child a").addClass("active").show(); //Activate first tab
-   //$(".tab_content #articles").hide(); //hide articles tab content
-   //$(".tab_content #portfolio").hide(); //hide portfolio tab content
-    $(".tab_content:first-child").show(); //Show first tab content
-
-    //On Click Event
-    $("ul.tabs li a").click(function () {
-
+        //On Click Event
+        $("ul.tabs li a").click(function () {
         $("ul.tabs li a").removeClass("active"); //Remove any "active" class
+        
         $(".tab_content_container div").hide();
 
         $(this).addClass("active"); //Add "active" class to selected tab
